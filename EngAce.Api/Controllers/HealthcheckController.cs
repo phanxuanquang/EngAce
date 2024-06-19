c982ccf2014f86c745b4f9fc7e57571a841ed3fb
Phan Xuân Quang <20521008@ms.uit.edu.vn>
2024-06-19 21:39:01 +0700

    add healthcheck

--- EngAce.Api/Controllers/HealthcheckController.cs
+++ EngAce.Api/Controllers/HealthcheckController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Entities.Enums;
+using Functions;
+using Helper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace EngAce.Api.Controllers
 {
@@ -7,6 +11,32 @@ namespace EngAce.Api.Controllers
     [ApiController]
     public class HealthcheckController : ControllerBase
     {
+        private readonly string _accessKey;
+        private readonly IMemoryCache _cache;
 
+        public HealthcheckController(IMemoryCache cache)
+        {
+            _cache = cache;
+            _accessKey = HttpContextHelper.GetAccessKey();
+        }
+
+        [HttpGet("Healthcheck")]
+        public async Task<ActionResult<string>> Healthcheck()
+        {
+            if (string.IsNullOrEmpty(_accessKey))
+            {
+                return Unauthorized("Missing Gemini API Key or Access Token");
+            }
+
+            try
+            {
+                var result = await HealthcheckScope.Healthcheck(_accessKey);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return Unauthorized(ex.Message);
+            }
+        }
     }
 }
---
+++ Functions/HealthcheckScope.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    public static class HealthcheckScope
+    {
+        public static async Task<string> Healthcheck(string apiKey)
+        {
+            var prompt = "Say 'Hello World' to me.";
+            try
+            {
+                return await Gemini.Generator.Generate(apiKey, prompt, false);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
