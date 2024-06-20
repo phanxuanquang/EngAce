3baae484e325ba041a25b5adf5389ecb4ef4c064
Phan Xuan Quang <qphan@mantu.com>
2024-06-20 21:01:16 +0700

    fix content

--- Entities/Enums/EnglishLevel.cs
+++ Entities/Enums/EnglishLevel.cs
@@ -4,13 +4,13 @@ namespace Entities.Enums
 {
     public enum EnglishLevel
     {
-        [Description("Beginner: Trình độ có thể tương đương IELST band 3.0 trở xuống")]
+        [Description("Beginner: Trình độ có thể tương đương IELST band 4.0 trở xuống")]
         Beginner = 1,
 
-        [Description("Intermediate: Trình độ có thể tương đương IELST band 4.0 đến 6.0")]
+        [Description("Intermediate: Trình độ có thể tương đương IELST band 4.5 đến 6.5")]
         Intermediate = 2,
 
-        [Description("Advanced: Trình độ có thể tương đương IELST band 6.5 trở lên")]
+        [Description("Advanced: Trình độ có thể tương đương IELST band 7.0 trở lên")]
         Advanced = 3,
     }
 }
