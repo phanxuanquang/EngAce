70ff957621688fb701d218774042a226976de10e
Hack Kiếm Sĩ <84692657+phanxuanquang@users.noreply.github.com>
2024-09-25 00:14:20 +0700

    Update GenerativeModel.cs

--- Gemini/GenerativeModel.cs
+++ Gemini/GenerativeModel.cs
@@ -7,10 +7,10 @@ namespace Gemini
         [Description("gemini-1.0-pro")]
         Gemini_10_Pro = 1,
 
-        [Description("gemini-1.5-pro")]
+        [Description("gemini-1.5-pro-latest")]
         Gemini_15_Pro = 2,
 
-        [Description("gemini-1.5-flash")]
+        [Description("gemini-1.5-flash-latest")]
         Gemini_15_Flash = 3,
     }
 }
