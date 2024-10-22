1ca23ea6fd9afc76069a51f7c3bd55d13d15f84a
PHAN Xuan Quang <20521008@ms.uit.edu.vn>
2024-10-22 23:13:17 +0700

    fix cors

--- EngAce.Api/Program.cs
+++ EngAce.Api/Program.cs
@@ -87,7 +87,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policyBuilder =>
     {
-        policyBuilder.WithOrigins("https://engace-app.azurewebsites.net", "https://engace.vercel.app")
+        policyBuilder.AllowAnyOrigin()
                      .AllowAnyHeader()
                      .AllowAnyMethod();
     });
